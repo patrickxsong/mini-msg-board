6532f3ba1bb397b7ed7808a805dd00de5f9000e3
patrick <patrickjksong@gmail.com>
2022-04-12 16:45:50 -0700

    Completed project

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+let time = require('date-fns/formatDistanceToNow');
+
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Mini Messageboard',
+    messages: messages,
+    format: time,
+  });
+});
+
+router.post('/new', (req, res) => {
+  let messageText = req.body.text;
+  let messageUser = req.body.user;
+
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect('/');
 });
 
 module.exports = router;
